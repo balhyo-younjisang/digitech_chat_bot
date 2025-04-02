4c65f47666f1f496e0f487c6d71e13b6902c2e67
balhyo-younjisang <yjs12180825@gmail.com>
2025-04-02 19:22:27 +0900

    feat: endpoint ( why-digitech, apply-middleschool-experience ) 추가

--- main.py
+++ main.py
@@ -15,6 +15,64 @@ class KakaoRequest(BaseModel):
 
 app = FastAPI()
 
+@app.post("/why-digitech")
+def why_digitech(request : Request):
+    return {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "basicCard": {
+                        "title": "WHY 디지텍?",
+                        "description": "빠르게 변화하는 글로벌 디지털 시대, \n 신산업 분야의 수요를 반영한 교육과 글로벌 취업 창업이 가능한 IT 분야 인재 육성 \n  학생이 주인공인 즐거운 학교, 재미있게 배우는 학교 서울디지텍고등학교",
+                        "thumbnail": {
+                            "imageUrl": "https://sdh.sen.hs.kr/dggb/module/image/selectDesignImageView.do?sitemapId=310558&usrimgId=75739"
+                        },
+                        "buttons": [
+                            {
+                            "label": "더 알아보기",
+                            "action": "webLink",
+                            "webLinkUrl": "https://sdh.sen.hs.kr/198663/subMenu.do"
+                            },
+                            {
+                                "label": "전화하기",
+                                "action": "phone",
+                                "phoneNumber": "02-798-3641"
+                            },
+                                {
+                                "label": "공유하기",
+                                "action": "share"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+
+@app.post("/apply-middleschool-experience")
+def apply_middleschool_experience(request : Request):
+    return {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "textCard": {
+                        "title" : '2025학년도 서울디지텍고등학교 1학기 "꿈을 찾는 진로체험!!" 신청 안내',
+                         "description": "2025학년도 서울디지텍고등학교 1학기에 진행하는 꿈을 찾는 진로 체험!! \n 4월 9일부터 시작하는 매주 수요일 14시~16시, 꿈을 찾는 체험 과정에 함께 해요",
+                         "buttons": [
+                            {
+                                "action": "webLink",
+                                "label": "신청하러 가기",
+                                "webLinkUrl": "https://naver.me/GbDu79HR"
+                            }
+                         ]
+                    }
+                }
+            ]
+        }
+    }
+
 @app.post("/message")
 async def message(request : Request):
     kakao_request = await request.json()
